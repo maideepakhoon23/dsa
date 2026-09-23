e525dbe6ed8cb37478df7435516b249da193c808
Deepak Joshi <deepakjoshi232006@gmail.com>
2026-09-23 16:26:52 +0530

    Time: 680 ms (85.76%), Space: 0B (100%) - LeetHub

---
+++ 0184-department-highest-salary/0184-department-highest-salary.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+select d.name as Department,e.name as Employee,e.salary as Salary from employee e left join department d on e.departmentId=d.id where (departmentId,salary) in (select departmentid,max(salary) from employee group by departmentid );
